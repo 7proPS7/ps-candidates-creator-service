90882acfe17027dc91778c4fa58effa14e48aa04
pawel <7proPS7@gmail.com>
2019-03-26 21:25:42 +0100

    #1 - Introduce skeleton project

---
+++ src/main/java/com/ps/candidates/creator/PsCandidatesCreatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ps.candidates.creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsCandidatesCreatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsCandidatesCreatorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ps/candidates/creator/PsCandidatesCreatorServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ps.candidates.creator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsCandidatesCreatorServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
